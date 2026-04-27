258ce01f14535cbd411123588d26a3d9b66ef9f2
Senior Full Stack Developer SCS (Ayezan Riaz) <ayezancoder93@gmail.com>
2026-04-27 18:55:07 +0500

    Update

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     origin.strip()
-    for origin in os.getenv("CORS_ALLOWED_ORIGINS", "http://127.0.0.1:5173,http://localhost:5173").split(",")
+    for origin in os.getenv("CORS_ALLOWED_ORIGINS", "https://eld-trip-planner-fronend.vercel.app").split(",")
     if origin.strip()
 ]
 
